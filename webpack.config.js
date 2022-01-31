ee2c77d9e616d8183765c351b8c6baedf90c7f32
lena-r-a <rizhankova.elena@gmail.com>
2022-01-31 18:35:56 +0300

    feat: add assets/fons pattern to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -60,6 +60,7 @@ const baseConfig = {
         new CopyPlugin({
             patterns: [
               { from: path.resolve(__dirname, './src/assets/images'), to: path.resolve(__dirname, './dist/assets/images') },
+              { from: path.resolve(__dirname, './src/assets/fonts'), to: path.resolve(__dirname, './dist/assets/fonts') },
             ],
           }),
     ],
