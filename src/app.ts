9fdb90085b7468a97f77bc24dc8b49af9e1818c9
Tatsiana <t_a_b@tut.by>
2022-02-01 17:43:25 +0300

    feat: implement menu work

---
+++ src/app.ts
@@ -0,0 +1,78 @@
+'use strict';
+import { ElBookPage } from './components/elBookPage';
+import { MainPage } from './components/mainPage';
+import { StatisticsPage } from './components/statisticsPage';
+import { GameSprintPage } from './components/games/sprintPage';
+import { GameChallengePage } from './components/games/challengePage';
+import { AutorizationPage } from './components/autorizationPage';
+import { Page } from './core/templates/page';
+import { Header } from './common/header';
+
+export const enum PageIds {
+  mainPage = 'mainPage',
+  statisticsPage = 'statisticsPage',
+  autorizationPage = 'autorizationPage',
+  gameChallengePage = 'gameChallengePage',
+  gameSprintPage = 'gameSprintPage',
+  elBookPage = 'ElBookPage',
+}
+
+export class App {
+  private static container: HTMLElement = document.body;
+
+  private initialPage: MainPage;
+
+  private header: Header;
+
+  private static defaultPageId = 'currentPage';
+
+  constructor() {
+    this.initialPage = new MainPage('autorizationPage');
+    this.header = new Header('header', 'header');
+  }
+
+  static renderNewPage(idPage: string) {
+    const currentPage = document.getElementById(App.defaultPageId);
+    if (currentPage) {
+      currentPage.remove();
+    }
+    let page: Page | null = null;
+    switch (idPage) {
+      case PageIds.mainPage:
+        page = new MainPage(idPage);
+        break;
+      case PageIds.elBookPage:
+        page = new ElBookPage(idPage);
+        break;
+      case PageIds.statisticsPage:
+        page = new StatisticsPage(idPage);
+        break;
+      case PageIds.autorizationPage:
+        page = new AutorizationPage(idPage);
+        break;
+      case PageIds.gameChallengePage:
+        page = new GameChallengePage(idPage);
+        break;
+      case PageIds.gameSprintPage:
+        page = new GameSprintPage(idPage);
+    }
+    if (page) {
+      const pageHTML = page.render();
+      pageHTML.id = App.defaultPageId;
+      App.container.append(pageHTML); //вставили новую страницу
+    }
+  }
+
+  private enableRouteChange() {
+    window.addEventListener('hashchange', () => {
+      const hash = window.location.hash.slice(1);
+      App.renderNewPage(hash);
+    });
+  }
+
+  public run() {
+    App.container.append(this.header.render());
+    App.renderNewPage('gameSprintPage');
+    this.enableRouteChange();
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 import './styles/main.scss';
+import { App } from './app';
 
+const app = new App();
+app.run();
