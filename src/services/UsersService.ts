87b2181976bf580dcbcaa5328dfa74901f8e8cba
lena-r-a <rizhankova.elena@gmail.com>
2022-02-03 19:21:55 +0300

    feat: add API requests

---
+++ src/services/UsersService.ts
@@ -0,0 +1,123 @@
+import { RestService } from './RestService';
+
+export interface IUserCreate {
+  name?: string;
+  email: string;
+  password: string;
+}
+
+export interface IUser {
+  id: string;
+  name: string;
+  email: string;
+}
+
+export interface IUserLogin {
+  message: string;
+  token: string;
+  refreshToken: string;
+  userId: string;
+  name: string;
+}
+
+export class UserService {
+  restService: RestService;
+
+  constructor() {
+    this.restService = new RestService();
+  }
+
+  public async createUser(user: IUserCreate): Promise<IUser | undefined> {
+    try {
+      const response = await this.restService.post<IUserCreate>('/users', user);
+      if (response.status === 417) alert('User already esists');
+      const result = await response.json();
+      return result;
+    } catch {
+      () => console.log('Bad request');
+    }
+  }
+
+  public async loginUser(user: IUserCreate): Promise<IUserLogin | undefined> {
+    try {
+      const response = await this.restService.post<IUserCreate>('/signin', user);
+      const result = await response.json();
+      return result;
+    } catch {
+      () => console.log('Bad request');
+    }
+  }
+
+  public async getUser(id: string, token: string): Promise<IUserCreate | undefined> {
+    const fullURL = `${this.restService.baseURL}/users/${id}`;
+    try {
+      const response = await fetch(fullURL, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
+      const result = await response.json();
+      return result;
+    } catch {
+      () => console.log('Bad request');
+    }
+  }
+
+  public async editUser(id: string, token: string, data: IUserCreate): Promise<IUserCreate | undefined> {
+    const fullURL = `${this.restService.baseURL}/users/${id}`;
+    try {
+      const response = await fetch(fullURL, {
+        method: 'PUT',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      const result = await response.json();
+      return result;
+    } catch {
+      () => console.log('Bad request');
+    }
+  }
+
+  public async deleteUser(id: string, token: string): Promise<IUserCreate | undefined> {
+    const fullURL = `${this.restService.baseURL}/users/${id}`;
+    try {
+      const response = await fetch(fullURL, {
+        method: 'DELETE',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
+      const result = await response.json();
+      return result;
+    } catch {
+      () => console.log('Bad request');
+    }
+  }
+
+  public async getUserToken(id: string): Promise<IUserLogin | undefined> {
+    const fullURL = `${this.restService.baseURL}/users/${id}/tokens`;
+    try {
+      const response = await fetch(fullURL, {
+        method: 'GET',
+        headers: {
+          // 'Authorization': `Bearer ${token}`,
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
+      const result = await response.json();
+      return result;
+    } catch {
+      () => console.log('Bad request');
+    }
+  }
+}
