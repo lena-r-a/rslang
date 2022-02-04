6080803a5124c1eff15b3ce4f251482ca3428c35
Tatsiana <t_a_b@tut.by>
2022-02-04 22:43:38 +0300

    feat: implement class SignUpPage

---
+++ src/components/autorizationPage/SignUpPage.ts
@@ -0,0 +1,30 @@
+import { Page } from '../../core/templates/page';
+import { SignUpForm } from './signUpForm';
+import './autorizationPage.scss';
+
+export class SignUpPage extends Page {
+  private form: SignUpForm;
+
+  private formWrapper: HTMLElement;
+
+  static TextObject = {
+    MainTitle: 'SignUpPage',
+  };
+
+  constructor(id: string) {
+    super(id);
+    this.container.classList.add('aut');
+    this.form = new SignUpForm();
+    this.formWrapper = document.createElement('div');
+    this.formWrapper.classList.add('aut__wrapper');
+  }
+
+  render() {
+    const title = this.createHeaderTitle(SignUpPage.TextObject.MainTitle);
+    title.classList.add('visually-hidden');
+    this.container.append(title);
+    this.formWrapper.append(this.form.render());
+    this.container.append(this.formWrapper);
+    return this.container;
+  }
+}
