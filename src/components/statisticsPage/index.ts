280550ff28d68403a243f8d3a1f338bed7204aee
Tatsiana <t_a_b@tut.by>
2022-02-12 00:15:33 +0300

    feat: implement statisticsPage component with shortStatistics

--- src/components/statisticsPage/index.ts
+++ src/components/statisticsPage/index.ts
@@ -1,19 +1,37 @@
-'use strict';
-
+import { Button } from '../../common/button';
 import { Page } from '../../core/templates/page';
+import { ShortTermStatistics } from './shortTermStatistics';
+import './statisticsPage.scss';
 
 export class StatisticsPage extends Page {
+  private shortTermStatBtn: HTMLElement;
+
+  private longTermStatBtn: HTMLElement;
+
+  private btnsWrapper: HTMLElement;
+
   static TextObject = {
     MainTitle: 'StatisticsPage',
   };
 
   constructor(id: string) {
     super(id);
+    this.container.classList.add('stat');
+    this.shortTermStatBtn = new Button('button', 'Статистика за текущий день', ['stat__btn', 'stat__btn--shortTerm']).rendor();
+    this.longTermStatBtn = new Button('button', 'долгострочная статистика', ['stat__btn', 'stat__btn--shortTerm']).rendor();
+    this.btnsWrapper = document.createElement('div');
+    this.btnsWrapper.classList.add('stat__btn-wrapper');
   }
 
-  render() {
+  public render() {
     const title = this.createHeaderTitle(StatisticsPage.TextObject.MainTitle);
+    title.classList.add('visually-hidden');
     this.container.append(title);
+    this.btnsWrapper.append(this.shortTermStatBtn);
+    this.btnsWrapper.append(this.longTermStatBtn);
+    this.container.append(this.btnsWrapper);
+    const shortTermStat = new ShortTermStatistics().render();
+    this.container.append(shortTermStat);
     return this.container;
   }
 }
