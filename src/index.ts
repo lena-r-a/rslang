975ba3d4f42600a3dffdcb9f1016babc55a18818
Tatsiana <t_a_b@tut.by>
2022-02-13 17:10:02 +0300

    feat: implement clearing statictics in case of overflow options

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { App } from './app';
+import { Statistics } from './states/statisticsState';
 // import { refreshUserToken } from './states/logInData';
 import './styles/main.scss';
 
@@ -6,3 +7,13 @@ const app = new App();
 app.run();
 
 // refreshUserToken();
+
+const data = {
+  newWords: 2,
+  rightAnsw: 5,
+  questions: 5,
+  session: 5,
+};
+
+// Statistics.updateStat('challenge', data);
+// Statistics.updateStat('learned', 7);
--- src/states/statisticsState.ts
+++ src/states/statisticsState.ts
@@ -18,8 +18,10 @@ export const statData: StatDataOptionalType = {
   },
 };
 
-export class StatisticsState {
+export class Statistics {
   private key: string;
+  //key 'challenge' | 'sprint' | 'learned'
+  //если ключ 'challenge' | 'sprint' - передаем как второй парамет объект типа StatDataGameType
 
   private statisticService: StatisticService;
 
@@ -37,10 +39,9 @@ export class StatisticsState {
       const result: StatDataType = await response.json();
       delete result.id;
       return result;
-      //todo 401 введен ошибочный токен 403 unautorised
     } else if (response.status === 401) {
       refreshUserToken();
-      // await this.getUserStat();
+      await this.getUserStat();
     } else if (response.status === 404) {
       const newUserStat: StatDataType = { learnedWords: 0, optional: {} };
       return newUserStat;
@@ -51,10 +52,10 @@ export class StatisticsState {
 
   public refreshUserStat(dataFromBack: StatDataType, currentDate: string): StatDataType {
     const backDataObj: StatDataOptionalType = dataFromBack.optional[currentDate];
+    console.log(backDataObj);
     if (this.key === 'learned' && typeof this.data === 'number') {
       console.log(this.data);
-      console.log(this.data);
-      backDataObj.learned = this.data;
+      backDataObj.learned += this.data;
     } else if ((this.key === 'sprint' || this.key === 'challenge') && typeof this.data !== 'number') {
       backDataObj[this.key].newWords += this.data.newWords;
       backDataObj[this.key].questions += this.data.questions;
@@ -70,44 +71,56 @@ export class StatisticsState {
     const newData = Object.assign({}, statData);
     console.log('newData');
     console.log(newData);
+    // todo записать данные в новую дату
     dataFromBack.optional[currentDate] = newData;
     return dataFromBack;
   }
 
-  private getEarlyStatPerDay(datesArr: string[]): string {
+  private getEarlyStatPerDay(datesArr: string[]): string | undefined {
     const savedDatesNumbers = datesArr.map((el) => Number(el.split(':').join('')));
-    //todo
-    return String(Math.min.apply(0, savedDatesNumbers));
+    console.log('savedDatesNumbers');
+    console.log(savedDatesNumbers);
+    const earlyNumber = Math.min.apply(0, savedDatesNumbers);
+    console.log(earlyNumber);
+    const earlyNumberIndex = savedDatesNumbers.findIndex((el) => el === earlyNumber);
+    if (datesArr[earlyNumberIndex]) {
+      return datesArr[earlyNumberIndex];
+    }
   }
 
-  public async updateUserStat() {
-    const dataFromBack: StatDataType | undefined = await this.getUserStat();
+  static async updateStat(key: string, data: StatDataGameType | number) {
+    const statState = new Statistics(key, data);
+    const dataFromBack: StatDataType | undefined = await statState.getUserStat();
     if (dataFromBack) {
       console.log('dataFromBack');
       console.log(dataFromBack);
-      const currentDate: string = this.getDate();
+      const currentDate: string = statState.getDate();
       const savedDates: string[] = Object.keys(dataFromBack.optional);
       console.log('savedDates');
       console.log(savedDates);
+      // удаляем наиболее ранние данные, если option переполнен
       if (savedDates.length > 8) {
-        const deletedDataKey = this.getEarlyStatPerDay(savedDates);
+        const deletedDataKey = statState.getEarlyStatPerDay(savedDates);
         console.log('deletedKey');
         console.log(deletedDataKey);
-        delete dataFromBack.optional[deletedDataKey];
+        if (deletedDataKey) {
+          delete dataFromBack.optional[deletedDataKey];
+        }
       }
       let updatedData: StatDataType;
       if (savedDates.includes(currentDate)) {
         // обновить данные за день
-        updatedData = this.refreshUserStat(dataFromBack, currentDate);
+        updatedData = statState.refreshUserStat(dataFromBack, currentDate);
         console.log('updatedData');
         console.log(updatedData);
       } else {
         // сохранить новые данные за день
-        updatedData = this.createNewStatPerDay(dataFromBack, currentDate);
+        const newData: StatDataType = statState.createNewStatPerDay(dataFromBack, currentDate);
+        updatedData = statState.refreshUserStat(newData, currentDate);
         console.log('updatedData');
         console.log(updatedData);
       }
-      await this.statisticService.upsertStatistics(logInData.userId!, logInData.token!, updatedData);
+      await statState.statisticService.upsertStatistics(logInData.userId!, logInData.token!, updatedData);
     }
   }
 
@@ -116,7 +129,7 @@ export class StatisticsState {
     const year = currentDate.getFullYear();
     const month = currentDate.getMonth();
     const day = currentDate.getDate();
-    return `${year}:${month}:${day}`;
+    return `${year}:${month < 10 ? '0' + month : month}:${day < 10 ? '0' + day : day}`;
   }
 }
 
