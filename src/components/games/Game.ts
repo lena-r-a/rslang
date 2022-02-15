e38e65074e5bcc3b2e6f2e3107569c5df903f0e2
whispermind <49441048+whispermind@users.noreply.github.com>
2022-02-15 19:14:22 +0200

    feat: add main sprint game interface

--- src/components/games/Game.ts
+++ src/components/games/Game.ts
@@ -2,15 +2,28 @@ import { Page } from '../../core/templates/page';
 import { IWord } from '../../services/WordsService';
 import { WordServise } from '../../services/WordsService';
 import getRandomInt from '../../common/getRandomInt';
-import './stylesheet.scss'
+import './stylesheet.scss';
 
-export abstract class Game extends Page{
-  title: string;
-  constructor(id: string, title: string, data?: IWord[]) {
+export abstract class Game extends Page {
+  protected title: string;
+
+  protected questionsList: IWord[] | null = null;
+
+  protected currentQuestion = 0;
+
+  protected results: boolean[] = [];
+
+  constructor(id: string, title: string, page?: number, group?: number) {
     super(id);
     this.title = title;
-    if (data) { this.startGame(data);
-    } else this.showMenu();
+    if (!page || !group) {
+      this.showMenu();
+    } else {
+      const ITEMS_RESPONSE = this.getGameItems(page, group);
+      ITEMS_RESPONSE.then((ITEMS) => {
+        if (ITEMS) this.startGame();
+      });
+    }
   }
 
   render() {
@@ -21,12 +34,12 @@ export abstract class Game extends Page{
     return this.container;
   }
 
-  abstract startGame(items: IWord[]): void;
+  abstract startGame(): void;
 
   private showMenu() {
     const MENU_CONTAINER = document.createElement('div');
     const DESCRIPTION = document.createElement('p');
-    const DIFFICULTY_SELECT = document.createElement('select');
+    const GROUP_SELECT = document.createElement('select');
     const START_BUTTON = document.createElement('button');
     const BUTTON_TEXT = 'старт';
     const DESCRIPTION_TEXT = 'Выберите сложность';
@@ -34,26 +47,27 @@ export abstract class Game extends Page{
       const OPTION = document.createElement('option');
       OPTION.textContent = String(i);
       OPTION.value = String(i);
-      DIFFICULTY_SELECT.append(OPTION);
+      GROUP_SELECT.append(OPTION);
     }
     START_BUTTON.textContent = BUTTON_TEXT;
     DESCRIPTION.textContent = DESCRIPTION_TEXT;
     MENU_CONTAINER.classList.add('game__menu');
-    MENU_CONTAINER.append(DESCRIPTION, DIFFICULTY_SELECT, START_BUTTON);
+    MENU_CONTAINER.append(DESCRIPTION, GROUP_SELECT, START_BUTTON);
     this.container.append(MENU_CONTAINER);
     START_BUTTON.addEventListener('click', async () => {
-      const ITEMS = await this.getGameItems(Number(DIFFICULTY_SELECT.value));
+      await this.getGameItems(Number(GROUP_SELECT.value));
       MENU_CONTAINER.remove();
-      if(ITEMS) this.startGame(ITEMS);
+      this.startGame();
     });
   }
 
-  async getGameItems(difficulty: number) {
+  private async getGameItems(group: number, page?: number) {
     const API = new WordServise();
     const MIN = 1;
     const MAX = 20;
-    const RANDOM_PAGE = getRandomInt(MIN, MAX);
-    const RESPONSE = await API.getWords(RANDOM_PAGE, difficulty);
-    return RESPONSE;
+    if (!page) page = getRandomInt(MIN, MAX);
+    const ITEMS = await API.getWords(page, group);
+    if (ITEMS) this.questionsList = ITEMS;
+    return ITEMS;
   }
 }
--- src/components/games/sprintPage/index.ts
+++ src/components/games/sprintPage/index.ts
@@ -1,13 +1,163 @@
 'use strict';
 import { Game } from '../Game';
-import { IWord } from '../../../services/WordsService';
+import getRandomInt from '../../../common/getRandomInt';
+import './sprintPage.scss';
 
 export class GameSprintPage extends Game {
-  constructor(id: string, data?: IWord[]){
-    const TITLE = `GameSprintPage`;
-    super(id, TITLE, data);
+  private invervalId: NodeJS.Timer | null = null;
+
+  private gameTime = 60;
+
+  private pointsGrowth = 10;
+
+  private readonly minGrowth = 10;
+
+  private readonly maxGrowth = 80;
+
+  private totalPoints = 0;
+
+  private stackCurrent = 0;
+
+  private readonly maxStackItemsAmount = 3;
+
+  private stackItems: HTMLCollection | null = null;
+
+  private stackDesciprtion: HTMLElement;
+
+  private sprintGameContainer: HTMLElement;
+
+  private question: HTMLElement;
+
+  private answer: HTMLElement;
+
+  private score: HTMLElement;
+
+  constructor(id: string, page?: number, group?: number, title = 'GameSprintPage') {
+    super(id, title, page, group);
+    this.sprintGameContainer = document.createElement('div');
+    this.stackDesciprtion = document.createElement('p');
+    this.question = document.createElement('p');
+    this.answer = document.createElement('p');
+    this.score = document.createElement('div');
+    this.container.append(this.sprintGameContainer);
   }
-  startGame(items: IWord[]): void {
-    
+
+  startGame(): void {
+    this.renderTimer();
+    this.renderStack();
+    this.renderScore();
+    this.renderQuestion();
+    this.renderControls();
+  }
+
+  renderStack() {
+    const CONTAINER = document.createElement('div');
+    const STACK_CONTAINER = document.createElement('div');
+    STACK_CONTAINER.classList.add('game__stack');
+    for (let i = 0; i < this.maxStackItemsAmount; i++) {
+      const item = document.createElement('div');
+      item.classList.add('game__stack-item');
+      STACK_CONTAINER.append(item);
+    }
+    this.stackItems = STACK_CONTAINER.children;
+    CONTAINER.classList.add('game__stack-heading');
+    this.stackDesciprtion.classList.add('game__stack-description');
+    this.stackDesciprtion.textContent = `${this.pointsGrowth} очков за ответ`;
+    CONTAINER.append(STACK_CONTAINER, this.stackDesciprtion);
+    this.sprintGameContainer.append(CONTAINER);
+  }
+
+  renderQuestion() {
+    const CONTAINER = document.createElement('div');
+    CONTAINER.append(this.question, this.answer);
+    this.sprintGameContainer.append(CONTAINER);
+    this.nextQuestion();
+  }
+
+  renderTimer() {
+    const TIMER_CONTAINER = document.createElement('div');
+    TIMER_CONTAINER.textContent = String(this.gameTime);
+    this.invervalId = setInterval(() => {
+      TIMER_CONTAINER.textContent = String(--this.gameTime);
+      if (!this.gameTime) this.renderResults();
+    }, 1000);
+    this.container.append(TIMER_CONTAINER);
+  }
+
+  renderControls() {
+    const CONTROLS_CONTAINER = document.createElement('div');
+    const RIGHT_BUTTON = document.createElement('button');
+    const WRONG_BUTTON = document.createElement('button');
+    RIGHT_BUTTON.textContent = 'Верно';
+    WRONG_BUTTON.textContent = 'Неверно';
+    CONTROLS_CONTAINER.classList.add('game__controls');
+    CONTROLS_CONTAINER.append(WRONG_BUTTON, RIGHT_BUTTON);
+    CONTROLS_CONTAINER.addEventListener('click', (e) => {
+      const CORRECT_TRANSLATE = this.questionsList![this.currentQuestion].wordTranslate;
+      if (e.target === RIGHT_BUTTON) {
+        if (this.answer.textContent === CORRECT_TRANSLATE) this.correctAnswer();
+        else this.incorrectAnswer();
+      } else if (e.target === WRONG_BUTTON) {
+        if (this.answer.textContent !== CORRECT_TRANSLATE) this.correctAnswer();
+        else this.incorrectAnswer();
+      }
+      this.updateStats();
+      this.currentQuestion++;
+      this.nextQuestion();
+    });
+    this.sprintGameContainer.append(CONTROLS_CONTAINER);
+  }
+
+  renderScore() {
+    this.sprintGameContainer.append(this.score);
+    this.updateStats();
+  }
+
+  renderResults() {
+    if (this.invervalId) clearInterval(this.invervalId);
+  }
+
+  nextQuestion() {
+    if (!this.questionsList) return;
+    const WORD_DATA = this.questionsList[this.currentQuestion];
+    const RANDOM_INT = getRandomInt(0, this.questionsList.length - 1);
+    const ANSWERS = [WORD_DATA.wordTranslate, this.questionsList[RANDOM_INT].wordTranslate];
+    const RANDOM_TRANSLATE = ANSWERS[getRandomInt(0, 1)];
+    this.question.textContent = WORD_DATA.word;
+    this.answer.textContent = RANDOM_TRANSLATE;
+  }
+
+  playSound(result: boolean) {}
+
+  correctAnswer() {
+    this.results.push(true);
+    this.totalPoints += this.pointsGrowth;
+    if (this.stackCurrent === this.maxStackItemsAmount && this.pointsGrowth < this.maxGrowth) {
+      this.pointsGrowth *= 2;
+      this.clearStack();
+    } else {
+      this.stackItems![this.stackCurrent++].classList.add('game__stack-item_filled');
+    }
+  }
+
+  incorrectAnswer() {
+    this.results.push(false);
+    this.clearStack();
+    if (this.pointsGrowth > this.minGrowth) {
+      this.pointsGrowth /= 2;
+    }
+  }
+  clearStack(){
+    this.stackCurrent = 0;
+    for (let i = 0; i < this.stackItems!.length; i++) {
+      this.stackItems![i].classList.remove('game__stack-item_filled');
+    }
+  }
+
+  updateStats(){
+    const DESCRIPTION = `${this.pointsGrowth} очков за ответ`;
+    const SCORE_DESCRIPTION = `Набрано очков: ${this.totalPoints}`;
+    this.stackDesciprtion.textContent = DESCRIPTION;
+    this.score.textContent = SCORE_DESCRIPTION;
   }
 }
